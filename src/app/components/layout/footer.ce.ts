920d5c817b6ecd0e936f931e61c3c9afc1d6603f
George Kalpakas <kalpakas.g@gmail.com>
2019-05-20 01:41:02 +0300

    fix(FooterCe): improve `FooterCe` styling

--- src/app/components/layout/footer.ce.ts
+++ src/app/components/layout/footer.ce.ts
@@ -4,19 +4,29 @@ import {BaseCe} from '../base.ce.js';
 export class FooterCe extends BaseCe {
   protected static readonly template = `
     <footer>
-      <small>
+      <span>
         Copyright &copy; 2019
         <a href="https://github.com/gkalpak">@gkalpak</a>.
         All rights reserved.
-      </small>
+      </span>
     </footer>
   `;
   protected static readonly style = `
+    :host { box-shadow: 0 0 5px; }
+
     footer {
-      background-color: linen;
+      align-items: center;
+      background-color: rgb(33, 33, 33);
+      color: lightgray;
       display: flex;
+      font-size: 0.75em;
       justify-content: center;
+      letter-spacing: 1px;
       padding: 2px 4px;
+      word-spacing: 2px;
     }
+
+    footer a { color: inherit; }
+    footer a:hover { color: white; }
   `;
 }
