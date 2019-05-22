ee4fc57475108c4fd9d1c2202ac10d0d0f986c2c
George Kalpakas <kalpakas.g@gmail.com>
2019-05-22 17:28:26 +0300

    cfg(karma): ensure invalid imports are not silently ignored

--- karma.conf.js
+++ karma.conf.js
@@ -11,8 +11,10 @@ module.exports = config => config.set({
     {pattern: 'test/unit/**/*.js', type: 'module'},
   ],
   frameworks: ['jasmine'],
+  middleware: ['exitOn404'],
   plugins: [
     'karma-*',
+    {'middleware:exitOn404': ['factory', exitOn404MiddlewareFactory]},
     {'reporter:jasmine-seed': ['type', JasmineSeedReporter]},
   ],
   reporters: [
@@ -23,6 +25,22 @@ module.exports = config => config.set({
 });
 
 // Helpers
+function exitOn404MiddlewareFactory(server) {
+  return function exitOn404Middleware(req, res, next) {
+    next();
+
+    // Ugly hack to ensure 404 errors don't cause silent failures.
+    // (E.g. if the import path is wrong in a spec file, the whole spec suite would be silently skipped.)
+    if (res.statusCode === 404) {
+      const importee = req.url.replace(/^\/base\//, '');
+      const importer = (req.headers.referer || '').
+        replace(new RegExp(`^${req.headers.origin || ''}/base/([^?#]*).*$`), '$1');
+
+      server.dieOnError(`Missing file '${importee}' (imported from '${importer}').`);
+    }
+  };
+}
+
 function JasmineSeedReporter(baseReporterDecorator) {
   baseReporterDecorator(this);
 
