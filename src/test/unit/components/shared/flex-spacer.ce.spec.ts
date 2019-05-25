db38e1e2204bd23228e4736573078281b1f2f673
George Kalpakas <kalpakas.g@gmail.com>
2019-05-25 18:50:16 +0300

    test(FlexSpacerCe): add unit tests

---
+++ src/test/unit/components/shared/flex-spacer.ce.spec.ts
@@ -0,0 +1,32 @@
+import {BaseCe} from '../../../../app/components/base.ce.js';
+import {FlexSpacerCe} from '../../../../app/components/shared/flex-spacer.ce.js';
+import {WIN} from '../../../../app/shared/constants.js';
+import {getNormalizedTextContent, setupCeContainer} from '../../test-utils.js';
+
+
+describe('FlexSpacerCe', () => {
+  const initCe = setupCeContainer();
+
+  beforeAll(() => FlexSpacerCe.register());
+
+  it('should extend `BaseCe`', async () => {
+    const elem = await initCe(FlexSpacerCe);
+
+    expect(elem).toEqual(jasmine.any(FlexSpacerCe));
+    expect(elem).toEqual(jasmine.any(BaseCe));
+  });
+
+  it('should have no content', async () => {
+    const elem = await initCe(FlexSpacerCe);
+    expect(getNormalizedTextContent(elem)).toBe('');
+  });
+
+  it('should be appropriately styled', async () => {
+    const elem = await initCe(FlexSpacerCe);
+    const style = WIN.getComputedStyle(elem);
+
+    expect(style.flexBasis).toBe('0px');
+    expect(style.flexGrow).toBe('1');
+    expect(style.flexShrink).toBe('1');
+  });
+});
