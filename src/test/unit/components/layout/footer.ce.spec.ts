306eef8dfac935d990475fd97bf087b0a1f4d07c
George Kalpakas <kalpakas.g@gmail.com>
2019-05-26 17:29:02 +0300

    test(FooterCe): add unit tests

---
+++ src/test/unit/components/layout/footer.ce.spec.ts
@@ -0,0 +1,31 @@
+import {BaseCe} from '../../../../app/components/base.ce.js';
+import {FooterCe} from '../../../../app/components/layout/footer.ce.js';
+import {getNormalizedTextContent, setupCeContainer} from '../../test-utils.js';
+
+
+describe('FooterCe', () => {
+  const initCe = setupCeContainer();
+
+  beforeAll(() => FooterCe.register());
+
+  it('should extend `BaseCe`', async () => {
+    const elem = await initCe(FooterCe);
+
+    expect(elem).toEqual(jasmine.any(FooterCe));
+    expect(elem).toEqual(jasmine.any(BaseCe));
+  });
+
+  it('should display a copyright notice', async () => {
+    const elem = await initCe(FooterCe);
+    expect(getNormalizedTextContent(elem)).toBe('Copyright © 2019 @gkalpak. All rights reserved.');
+  });
+
+  it('should contain a link to gkalpak\'s GitHub profile', async () => {
+    const elem = await initCe(FooterCe);
+    const link = elem.shadowRoot.querySelector('a')!;
+
+    expect(link).not.toBeNull();
+    expect(link.textContent).toBe('@gkalpak');
+    expect(link.href).toBe('https://github.com/gkalpak');
+  });
+});
