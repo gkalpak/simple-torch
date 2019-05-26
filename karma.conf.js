cbe3facdc00a24f0d86ec7da8b1482b1af1b1f34
George Kalpakas <kalpakas.g@gmail.com>
2019-05-26 22:01:36 +0300

    cfg(karma): add middleware for handling dummy requests

--- karma.conf.js
+++ karma.conf.js
@@ -18,10 +18,14 @@ module.exports = config => config.set({
     {pattern: 'out/test/unit/**/*.js', type: 'module'},
   ],
   frameworks: ['jasmine'],
-  middleware: ['exitOn404'],
+  middleware: [
+    'dummy-request',
+    'exit-on-404',
+  ],
   plugins: [
     'karma-*',
-    {'middleware:exitOn404': ['factory', exitOn404MiddlewareFactory]},
+    {'middleware:dummy-request': ['factory', dummyRequestMiddlewareFactory]},
+    {'middleware:exit-on-404': ['factory', exitOn404MiddlewareFactory]},
     {'reporter:jasmine-seed': ['type', JasmineSeedReporter]},
   ],
   preprocessors: {'out/**/*.js': ['sourcemap']},
@@ -34,18 +38,37 @@ module.exports = config => config.set({
 });
 
 // Helpers
+function dummyRequestMiddlewareFactory() {
+  return function dummyRequestMiddleware(req, res, next) {
+    const match = /^(?:\/base)?(\/dummy\/.+)$/.exec(req.url);
+
+    if (!match) {
+      next();
+    } else {
+      const body = `Dummy content. (URL: ${match[1]})`;
+      res.writeHead(200, {
+        'Content-Length': body.length,
+        'Content-Type': 'text/plain',
+      });
+      res.end(body);
+    }
+  };
+}
+
 function exitOn404MiddlewareFactory(server) {
   return function exitOn404Middleware(req, res, next) {
     next();
 
-    // Ugly hack to ensure 404 errors don't cause silent failures.
+    // Ugly hack mainly to ensure 404 errors don't cause silent failures.
     // (E.g. if the import path is wrong in a spec file, the whole spec suite would be silently skipped.)
     if (res.statusCode === 404) {
-      const importee = req.url.replace(/^\/base\//, '');
-      const importer = (req.headers.referer || '').
+      const requestee = req.url.replace(/^(?:\/base)?\//, '');
+      const requester = (req.headers.referer || '').
         replace(new RegExp(`^${req.headers.origin || ''}/base/([^?#]*).*$`), '$1');
 
-      server.dieOnError(`Missing file '${importee}' (imported from '${importer}').`);
+      server.dieOnError(
+        `Missing resource '${requestee}' (requested from '${requester}').\n` +
+        `If you want to make a dummy request, start the URL with '/dummy/' (e.g. '/dummy/${requestee}').`);
     }
   };
 }
