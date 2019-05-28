f95013f8aedc33de6ca8ffbc38e5e7344ed03aa0
George Kalpakas <kalpakas.g@gmail.com>
2019-05-28 18:45:49 +0300

    fix(scripts): ensure `generate-app.js` exits with a non-zero code when something goes wrong

--- scripts/generate-app.js
+++ scripts/generate-app.js
@@ -18,28 +18,33 @@ _main(process.argv.slice(2));
 
 // Helpers
 async function _main(args) {
-  const production = args.includes('--production');
-  const version = process.env.npm_package_version;
+  try {
+    const production = args.includes('--production');
+    const version = process.env.npm_package_version;
 
-  // Copy files.
-  sh.mkdir('-p', OUT_DIR);
-  sh.cp('-r', 'src/!(app|test)', OUT_DIR);
+    // Copy files.
+    sh.mkdir('-p', OUT_DIR);
+    sh.cp('-r', 'src/!(app|test)', OUT_DIR);
 
-  // Replace ENV placeholders.
-  sh.sed('-i', /<PLACEHOLDER:PRODUCTION>/g, `${production}`, OUT_INDEX_PATH);
-  sh.sed('-i', /<PLACEHOLDER:VERSION>/g, `${version}`, OUT_INDEX_PATH);
+    // Replace ENV placeholders.
+    sh.sed('-i', /<PLACEHOLDER:PRODUCTION>/g, `${production}`, OUT_INDEX_PATH);
+    sh.sed('-i', /<PLACEHOLDER:VERSION>/g, `${version}`, OUT_INDEX_PATH);
 
-  // In production mode...
-  if (production) {
-    // Replace SW placeholders.
-    const files = swFindFilesToCache();
-    const hashes = await swComputeHashes(files);
+    // In production mode...
+    if (production) {
+      // Replace SW placeholders.
+      const files = swFindFilesToCache();
+      const hashes = await swComputeHashes(files);
 
-    const swHashRepl = await hash(hashes.join());
-    const filesToCacheRepl = swCreateFilesToCacheReplacement(files, hashes);
+      const swHashRepl = await hash(hashes.join());
+      const filesToCacheRepl = swCreateFilesToCacheReplacement(files, hashes);
 
-    sh.sed('-i', /<PLACEHOLDER:SW_HASH>/g, swHashRepl, OUT_SW_PATH);
-    sh.sed('-i', /'<PLACEHOLDER:FILES_TO_CACHE>': ''/g, filesToCacheRepl, OUT_SW_PATH);
+      sh.sed('-i', /<PLACEHOLDER:SW_HASH>/g, swHashRepl, OUT_SW_PATH);
+      sh.sed('-i', /'<PLACEHOLDER:FILES_TO_CACHE>': ''/g, filesToCacheRepl, OUT_SW_PATH);
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
   }
 }
 
