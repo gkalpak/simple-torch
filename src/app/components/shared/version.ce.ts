37ef108ba0c863b25410c578a17285e278cf67a1
George Kalpakas <kalpakas.g@gmail.com>
2019-05-29 11:56:46 +0300

    feat(VersionCe): link to the corresponding commit on GitHub

--- src/app/components/shared/version.ce.ts
+++ src/app/components/shared/version.ce.ts
@@ -3,5 +3,16 @@ import {BaseCe} from '../base.ce.js';
 
 
 export class VersionCe extends BaseCe {
-  protected static readonly template = `v${ENV.version}`;
+  protected static readonly template = `
+    <a href="${ENV.repoUrl}/commit/${ENV.sha.slice(0, 9)}" title="SHA: ${ENV.sha}">
+      v${ENV.version}
+    </a>
+  `;
+  protected static readonly style = `
+    a {
+      color: inherit;
+      text-decoration: none;
+    }
+    a:hover { color: white; }
+  `;
 }
--- src/test/unit/components/shared/version.ce.spec.ts
+++ src/test/unit/components/shared/version.ce.spec.ts
@@ -19,4 +19,14 @@ describe('VersionCe', () => {
     const elem = await initCe(VersionCe);
     expect(getNormalizedTextContent(elem)).toBe('v1.33.7-foo');
   });
+
+  it('should contain a link to the corresponding commit SHA', async () => {
+    const elem = await initCe(VersionCe);
+    const link = elem.shadowRoot.querySelector('a')!;
+
+    expect(link).not.toBeNull();
+    expect(link.textContent!.trim()).toBe('v1.33.7-foo');
+    expect(link.href).toBe('https://pro.vid.er/my/repo/commit/a1b2c3d4e');
+    expect(link.title).toBe('SHA: a1b2c3d4e5f');
+  });
 });
